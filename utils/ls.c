0a6c1ebe34e2d7850a999d11c60a70f818ce5df1
Setzer <setzerrj@stu.cs.jmu.edu>
2022-09-26 20:52:09 -0400

    Semi-Working ls body

--- utils/ls.c
+++ utils/ls.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 static void usage (void);
 
 int
 main (int argc, char *argv[])
 {
+
+  int fd[2];
+  pipe (fd);
+  pid_t child_pid = fork();
+  if (child_pid < 0)
+    return EXIT_FAILURE;
+  if (child_pid == 0)
+    {
+      close (fd[0]);
+      dup2 (fd[1], STDOUT_FILENO);
+      execlp ("ls", "ls");
+    }
+  close (fd[1]);
+  char *buffer[1000];
+  read (fd[0], buffer, sizeof (buffer));
+  printf ("%s", buffer);
   return EXIT_SUCCESS;
 }
 
